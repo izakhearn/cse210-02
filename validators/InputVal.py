ca00d73fdf10a4a551535b9dbe01109237865e03
Izak Hern <izakhearn@gmail.com>
2022-09-24 23:21:05 +0200

    Updated Comments in Files

--- validators/InputVal.py
+++ validators/InputVal.py
@@ -1,18 +1,36 @@
 class InputValidation: 
     '''
-    This class is used to valdate the input of the user and retrun a true or false.
+    This class is used to validate the input of the user and return a true or false
+    when the input is valid or invalid.
     '''
 
     def __init__(self):
-        self.input_type = ""
+         '''
+        The class constructor.
+        Args:
+            self (InputValidation): an instance of InputValidation.
+        '''
     
     def YesNo(self,user_input):
+        '''
+        The Method that checks if the user input is a yes or no.
+        Args:
+            self (InputValidation): an instance of InputValidation.
+            user_input (string): The user input.
+        '''
         if user_input == "Y" or user_input == "y" or user_input == "N" or user_input == "n":
             return True
         else:
             return False
 
     def Choices(self,user_input,choices):
+        '''
+        The Method that checks if the user input is a yes or no.
+        Args:
+            self (InputValidation): an instance of InputValidation.
+            user_input (string): The user input.
+            choices (list): The list of choices a user can make.
+        '''
         if user_input in choices:
             return True
         else:
